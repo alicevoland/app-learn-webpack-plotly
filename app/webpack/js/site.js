5b5573bf42325bf4d900d64edb1b46e98ad40d47
Mathieu Voland <mathieu.voland@gmail.com>
2020-10-22 19:41:09 +0200

    Hello Plotly

--- app/webpack/js/site.js
+++ app/webpack/js/site.js
@@ -1,3 +1,14 @@
 import 'jquery'
 import 'popper.js'
 import 'bootstrap'
+
+
+var Plotly = require('plotly.js');
+console.log(Plotly.BUILD);
+var TESTER = document.getElementById('tester');
+Plotly.plot(TESTER, [{
+  x: [1, 2, 3, 4, 5],
+  y: [1, 2, 4, 8, 16]
+}], {
+  margin: { t: 0 }
+}, { showSendToCloud: true });
