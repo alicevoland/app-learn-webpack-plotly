45be253bec4753e83055f702a39fb1e95c1ae3b1
Mathieu Voland <mathieu.voland@gmail.com>
2020-10-28 23:27:12 +0100

    Simple Navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,23 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
   before_action :set_locale
 
   private
 
   def set_locale
-    if !params[:locale]
-      I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
+    I18n.locale = if !params[:locale]
+                    http_accept_language.compatible_language_from(I18n.available_locales)
+                  else
+                    params[:locale]
+                  end
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      'layouts/devise'
     else
-      I18n.locale = params[:locale]
+      'layouts/application'
     end
   end
 end
